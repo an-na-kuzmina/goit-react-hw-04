33b74ea55bf7a4336f2ec887b5fd2213b8b326ed
an-na-kuzmina <anna.bychkova2008@gmail.com>
2024-10-03 14:20:03 +0300

    done work

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,88 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
+import { fetchImages } from './services/unsplash-api';
+import toast from 'react-hot-toast';
+import SearchBar from './components/SearchBar/SearchBar';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import Loader from './components/Loader/Loader';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
+import ImageModal from './components/ImageModal/ImageModal';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [photos, setPhotos] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setisError] = useState(false);
+  const [isModalOpened, setIsModalOpened] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [modalData, setModalData] = useState({});
+
+  useEffect(() => {
+    if (!searchQuery) {
+      return;
+    }
+    const getPhotos = async () => {
+      try {
+        setisError(false);
+        setIsLoading(true);
+        const data = await fetchImages(page, searchQuery);
+        if (data.results.length === 0) {
+          toast('No results. Change your search query');
+        }
+        setPhotos(prev => [...prev, ...data.results]);
+        setTotalPages(data.total_pages);
+      } catch {
+        setisError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getPhotos();
+  }, [page, searchQuery]);
+
+  const handleChangePage = () => {
+    setPage(prev => prev + 1);
+  };
+
+  const handleSetSearchQuery = query => {
+    console.log(query);
+    if (query === searchQuery) {
+      return;
+    }
+    setSearchQuery(query);
+    setPhotos([]);
+    setPage(1);
+  };
+
+  const handleModalOpening = modalData => {
+    setIsModalOpened(true);
+    setModalData(modalData);
+  };
+
+  const handleModalClosing = () => {
+    setIsModalOpened(false);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Image Search: react hw 04</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SearchBar setSearchQuery={handleSetSearchQuery} />
+      {!!photos.length && (
+        <ImageGallery photos={photos} onImageClick={handleModalOpening} />
+      )}
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      {!!photos.length && page < totalPages && (
+        <LoadMoreBtn onClick={handleChangePage} />
+      )}
+      {isModalOpened && (
+        <ImageModal
+          isModalOpened={isModalOpened}
+          closeModal={handleModalClosing}
+          modalData={modalData}
+        />
+      )}
     </>
   );
 }
